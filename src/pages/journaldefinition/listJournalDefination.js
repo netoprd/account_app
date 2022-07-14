cf9b183a0c243fc69992cd752f485ab7b1de83b9
Stanizlauz <izukachibuzor4@gmail.com>
2022-07-14 09:45:59 +0100

    init

--- src/pages/journaldefinition/listJournalDefination.js
+++ src/pages/journaldefinition/listJournalDefination.js
@@ -36,7 +36,7 @@ export default function ListJournalDefination() {
         // getj()
     }, []);
     console.log('ffffff', journal )
-    const deleteJournal = (id) => {
+    const deleteJournal = (journalguid) => {
         try {
             Swal.fire({
                 title: 'Are you sure?',
@@ -47,8 +47,12 @@ export default function ListJournalDefination() {
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Yes, delete it!'
             }).then(async (result) => {
+                // result = journal.find((x) => x.journalguid === journalguid);
                 if (result.isConfirmed) {
-                    // const result = await api.StockAdjustment.delete(id);
+                      result = dbData.deletejournaldefination(journalguid, setDeleteList);
+                     console.log({deleteList})
+        const response = dbData.getalljuornaldiscription(setJournal)
+
                     // // notifySuccess(result.sucessMessage)
                     // if (result.sucessMessage) {
                     //     const response = await api.StockAdjustment.load(page, limit, "", documentType)
@@ -129,7 +133,7 @@ export default function ListJournalDefination() {
                                                             {!detail?.approvedBy && <>
                                                                 <Link to={`/editjournaldef/${detail.journalguid}`}><i className="ti-pencil btn-icon-append text-success" title="Edit" /></Link>
                                                                 {/* <Link to={`/stockreceiptview/${detail?.id}`}><i className="ti-trash btn-icon-append text-danger" title="Delete" /></Link> */}
-                                                                <i className="ti-trash btn-icon-append text-danger" onClick={() => deleteJournal(detail?.id)} title="Delete" />
+                                                                <i className="ti-trash btn-icon-append text-danger" onClick={() => deleteJournal(detail.journalguid)} title="Delete" />
                                                             </>}
                                                         </div>
                                                     </td>
